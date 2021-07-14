62b1a9fd9df1cb594e7b90b72c83db2dc3f37b26
Pedro Ivo <engseg.pedroivo@gmail.com>
2021-07-14 14:33:17 -0300

    Começando a imprementar o tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -7,13 +7,9 @@ namespace xadrez_console
     {
         static void Main(string[] args) 
         {
-            Posicao p = new Posicao(3, 4);
-            Console.WriteLine("Posição " + p);
+            Tabuleiro tabuleiro = new Tabuleiro(8, 8);
 
-
-
-            Console.ReadLine();
             
         }
     }
-}
+} 
